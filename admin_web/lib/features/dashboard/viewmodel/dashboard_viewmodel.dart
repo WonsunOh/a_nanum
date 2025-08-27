764e9741a4405e9d81d25b6e6895d70bdeee8493
gruto <gruto@naver.com>
2025-08-28 01:40:36 +0900

    Add GitHub Actions workflow for admin deployment

--- admin_web/lib/features/dashboard/viewmodel/dashboard_viewmodel.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../data/models/dashboard_metrics_model.dart';
-import '../../../data/repositories/dashboard_repository.dart';
-
-final dashboardViewModelProvider = FutureProvider.autoDispose<DashboardMetrics>((ref) {
-  return ref.watch(dashboardRepositoryProvider).fetchMetrics();
-});
