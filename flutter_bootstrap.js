a4dd80a092c1a14c29a7116c0fc1e0415164a828
WonsunOh <WonsunOh@users.noreply.github.com>
2025-08-30 15:37:21 +0000

    deploy: 77f33c607a3f1f6173cdeb80a04d92825bb04ed4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1391249810"
+    serviceWorkerVersion: "129135602"
   }
 });
